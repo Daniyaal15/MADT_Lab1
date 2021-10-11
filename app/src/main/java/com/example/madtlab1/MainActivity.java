c92b125ce388f3ce02f22c4a906db318ad5b6fd0
Daniyaal15 <spyder103@gmail.com>
2021-10-11 03:15:28 +0300

    The functionality of button was added, on pressing the button the text changes.

---
+++ app/src/main/java/com/example/madtlab1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.madtlab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView TxtViewId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.TxtViewId = findViewById(R.id.TxtViewId);
+    }
+
+    public void BtnClk(View view) {
+        this.TxtViewId.setText("Text Changed :D");
+    }
+}
